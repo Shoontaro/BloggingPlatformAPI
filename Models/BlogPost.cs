b5d0fc384f9a7498b6b40247764368165034c644
Shoontaro <tishinadw@gmail.com>
2026-09-03 23:58:09 +0300

    create BlogPost model

---
+++ Models/BlogPost.cs
@@ -0,0 +1,13 @@
+﻿namespace BloggingPlatformAPI.Models
+{
+    public class BlogPost
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string content { get; set; }
+        public string category { get; set; }
+        public string[] tags { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+    }
+}
